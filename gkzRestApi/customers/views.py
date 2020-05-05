b5bedf4ab38b04fef7ab1bda14b76d9725db56e5
Cecile-Hirschauer <cecile.h.75@gmail.com>
2020-05-05 15:06:13 +0200

    Modify model

--- gkzRestApi/customers/views.py
+++ gkzRestApi/customers/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONparser
+from rest_framework import status
 
-# Create your views here.
+from .models import Customer
+from .serializers import CustomerSerializers
+
+
+@csrf_exempt
+def customer_list(request):
+    if request.method == 'GET':
+        customers = Customers.objects.all()
+        customers_serializer = Cust
